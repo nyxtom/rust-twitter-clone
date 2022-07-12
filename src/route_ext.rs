f8c4fb440adcfa41cbd392e6fc5b0206ee535162
Thomas Holloway <nyxtom@gmail.com>
2022-07-12 15:02:29 -0500

    Add support for route extensions on authenticated middleware

---
+++ src/route_ext.rs
@@ -0,0 +1,36 @@
+use async_trait::async_trait;
+use tide::{Middleware, Next, Redirect, Request, Route};
+
+use crate::prelude::RequestExt;
+
+pub trait RouteExt {
+    fn authenticated(&mut self) -> &mut Self;
+}
+
+impl<'a, State: Clone + Send + Sync + 'static> RouteExt for Route<'a, State> {
+    fn authenticated(&mut self) -> &mut Self {
+        self.with(AuthenticatedMiddleware {});
+        self
+    }
+}
+
+pub struct AuthenticatedMiddleware {}
+
+#[async_trait]
+impl<State> Middleware<State> for AuthenticatedMiddleware
+where
+    State: Clone + Send + Sync + 'static,
+{
+    async fn handle(&self, request: Request<State>, next: Next<'_, State>) -> tide::Result {
+        if !request.is_authenticated() {
+            // Redirect::new(/login?redirect=url)
+            // Redirect::new(/)
+            // 401 Unauthorized
+            // 403 Forbidden
+            return Ok(Redirect::new("/").into());
+            //return Ok(Response::new(StatusCode::Unauthorized));
+        }
+
+        return Ok(next.run(request).await);
+    }
+}
